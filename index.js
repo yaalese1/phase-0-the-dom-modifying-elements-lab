7a0345a24ef8d55c545b2b3e8bbb14ed0203e8d5
Yetunde Alese <yaalese1@gmail.com>
2022-11-02 22:13:49 -0400

    commit tree 10f6a92376eefb6d57d8e10fb89e1b56127103d8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main")
+element.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "YAYA is the champion"
